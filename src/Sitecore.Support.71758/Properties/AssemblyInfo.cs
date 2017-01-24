cc1440facf28846a2d1543eb0282e149c469706b
Anton Kechashin <atk@sitecore.net>
2017-01-24 16:12:00 +0200

    Initial version

---
+++ src/Sitecore.Support.71758/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.71758")]
+[assembly: AssemblyProduct("Sitecore.Support.71758")]
+[assembly: ComVisible(false)]
